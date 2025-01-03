f962b730012c12e698625a910cd3d982f14e9feb
MichaelEight <michaleight822@gmail.com>
2025-01-03 21:03:04 +0100

    feat: Create folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import styles from './AppStyles';
 
 export default function App() {
   const [count, setCount] = useState(0);
@@ -15,11 +16,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
